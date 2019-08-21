ca3e00447b3349b1183fcf9c98951bfd2f0a3ad3
Denny Tek <tek.denny@gmail.com>
2019-08-21 09:52:50 -0400

    Update js and css dependencies and clean up scripts

--- source/_js/scripts.js
+++ source/_js/scripts.js
@@ -1,21 +1,16 @@
 // import Footer from './Footer';
 import Overlay from './overlay';
-import Modal from './modal';
 import Nav from './nav';
 import Scroll from './scroll';
 import Search from './search';
-import Slider from './slider';
 import Sticky from './sticky';
 import Tooltip from './tooltip';
 import Utils from './utils';
 
-// Footer.init();
-Modal.init();
 Nav.init();
 Overlay.init();
 Scroll.init();
 Search.init();
-Slider.init();
 Sticky.init();
 Tooltip.init();
 Utils.markdownLinksNewPage();

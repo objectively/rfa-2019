371cf997c2a47f6bd9f0654b16eb2b4a5d9251ec
Denny Tek <tek.denny@gmail.com>
2019-08-30 13:56:11 -0400

    Filter with attribute
    Filter by classname

--- source/_js/filter.js
+++ source/_js/filter.js
@@ -1,7 +1,37 @@
 const Filter = {
+  filterByAttribute() {
+    const hiddenClass = 'examples-wrapper--hidden';
+    document.querySelector('.issues-dropdown-filter__select').addEventListener('change', e => {
+      const selectedOption = e.currentTarget.options[e.currentTarget.selectedIndex];
+      const selectedIssue = selectedOption.value;
+      document.querySelectorAll('.examples-wrapper').forEach(el => {
+        const elIssues = el.getAttribute('data-examples');
+        if (elIssues.indexOf(selectedIssue) == -1) {
+          el.classList.add(hiddenClass);
+        } else {
+          el.classList.remove(hiddenClass);
+        }
+      });
+    });
+  },
+  filterByClassName() {
+    const hiddenClass = 'examples-wrapper--hidden';
+    document.querySelector('.issues-dropdown-filter__select').addEventListener('change', e => {
+      const selectedOption = e.currentTarget.options[e.currentTarget.selectedIndex];
+      const selectedIssue = selectedOption.value;
+      document.querySelectorAll('.examples-wrapper').forEach(el => {
+        if (el.classList.contains(selectedIssue)) {
+          el.classList.remove(hiddenClass);
+        } else {
+          el.classList.add(hiddenClass);
+        }
+      });
+    });
+  },
   init() {
-    console.log('hi');
+    this.filterByClassName();
+    // this.filterByAttribute();
   }
-}
+};
 
 module.exports = Filter;

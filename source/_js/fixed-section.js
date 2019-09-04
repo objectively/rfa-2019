48afd6ca6296d7597ceebe01948ef228058a67ee
Denny Tek <tek.denny@gmail.com>
2019-09-04 16:20:56 -0400

    Clean up fixed section js

--- source/_js/fixed-section.js
+++ source/_js/fixed-section.js
@@ -1,44 +1,14 @@
 const FixedSection = {
   fixIndexTable() {
-    console.log('scroll', $('.fixed-section'));
-
-    const $window = $(window);
-    if ($('.fixed-section').length > 0) {
-      const $fixedSection = $('.fixed-section');
-      const elTop = $fixedSection.offset().top;
-      // const margin = $('.fixed-section__cell').height();
-      const margin = 50;
-
-      $window.scroll(() => {
-        $fixedSection.toggleClass('fixed', $window.scrollTop() > elTop);
-        if ($window.scrollTop() > elTop) {
-          $fixedSection.addClass('fixed');
-          $('.fixed-section__row:first-of-type').css('margin-top', margin);
-          const fixedSectionWrapper = document.querySelector('.fixed-section-wrapper').getBoundingClientRect();
-          const fixedSectionTop = Math.abs(fixedSectionWrapper.top) + margin / 2;
-          const fixedSectionHeight = fixedSectionWrapper.height;
-          // when offset top of wrapper = height of wrapper remove fixed
-          if (fixedSectionHeight <= Math.abs(fixedSectionTop)) {
-            $fixedSection.removeClass('fixed');
-          }
-        } else {
-          $fixedSection.removeClass('fixed');
-          $('.fixed-section__row:first-of-type').css('margin-top', 0);
-        }
-      });
-    }
-  },
-  init() {
     const $window = $(window);
     if ($('.fixed-section').length > 0) {
       const $fixedSection = $('.fixed-section');
       const $fixedSectionWrapper = $('.fixed-section-wrapper');
-
       let elTop = $fixedSection.offset().top;
       // reassign elTop when read more is expanded
       $('.read-more').on('click', () => {
         elTop = $fixedSection.offset().top;
-      })
+      });
       const margin = 50;
 
       $window.scroll(() => {
@@ -49,17 +19,21 @@ const FixedSection = {
           const fixedSectionTop = Math.abs(fixedSectionWrapper.top) + margin;
           const fixedSectionHeight = fixedSectionWrapper.height;
           // when offset top of wrapper = height of wrapper remove fixed
-          $('.content-section').css('margin-top', $('.fixed-section').height())
-          $('.examples-grid__content-row:first-of-type .examples-grid__content-cell').css('padding-top','7rem');
-            fixedSectionHeight <= Math.abs(fixedSectionTop) ? $fixedSection.css('opacity', 0) : $fixedSection.css('opacity', 1)
+          $('.content-section').css('margin-top', $('.fixed-section').height());
+          $('.examples-grid__content-row:first-of-type .examples-grid__content-cell').css('padding-top', '7rem');
+          fixedSectionHeight <= Math.abs(fixedSectionTop)
+            ? $fixedSection.css('opacity', 0)
+            : $fixedSection.css('opacity', 1);
         } else {
           $fixedSection.removeClass('fixed');
           $('.content-section').css('margin-top', 'initial');
-          $('.examples-grid__content-row:first-of-type .examples-grid__content-cell').css('padding-top','3rem');
+          $('.examples-grid__content-row:first-of-type .examples-grid__content-cell').css('padding-top', '3rem');
         }
       });
     }
-
+  },
+  init() {
+    this.fixIndexTable();
   }
 };
 
